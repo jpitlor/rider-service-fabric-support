e1e30777b22e53d3ba0c9939a03e478ea799ca11
Jordan Pitlor <jordan.pitlor@avasure.com>
2021-03-26 09:56:22 -0400

    add some project logic

--- src/dev/pitlor/rider_service_fabric_support/ServiceFabricRunConfiguration.java
+++ src/dev/pitlor/rider_service_fabric_support/ServiceFabricRunConfiguration.java
@@ -1,25 +1,60 @@
 package dev.pitlor.rider_service_fabric_support;
 
 import com.intellij.execution.Executor;
-import com.intellij.execution.configurations.*;
+import com.intellij.execution.configurations.ConfigurationFactory;
+import com.intellij.execution.configurations.RunConfigurationBase;
+import com.intellij.execution.configurations.RunProfileState;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public class ServiceFabricRunConfiguration extends RunConfigurationBase<RunProfileState> {
+	final private String suggestedSfProjFolder;
+	final private String[] publishProfileOptions;
+
 	public String sfProjFolder;
 	public String publishProfile;
 
 	protected ServiceFabricRunConfiguration(Project project, ConfigurationFactory factory, String name) {
 		super(project, factory, name);
+
+		Optional<VirtualFile> maybeSfFolder = Arrays.stream(ProjectRootManager.getInstance(project).getContentRoots())
+			.filter(d -> d.isDirectory() && Arrays.stream(d.getChildren()).anyMatch(f -> f.getName().endsWith(".sfproj")))
+			.findFirst();
+
+		if (maybeSfFolder.isEmpty()) {
+			// There isn't a SF project in the open project
+			suggestedSfProjFolder = "";
+			publishProfileOptions = new String[] {};
+			return;
+		}
+
+		VirtualFile sfFolder = maybeSfFolder.get();
+		suggestedSfProjFolder = sfFolder.getName();
+
+		VirtualFile publishProfiles = sfFolder.findChild("PublishProfiles");
+		if (publishProfiles == null || !publishProfiles.isDirectory()) {
+			// There is a SF project, but it is malformed
+			publishProfileOptions = new String[] {};
+			return;
+		}
+
+		publishProfileOptions = Arrays.stream(publishProfiles.getChildren())
+			.map(VirtualFile::getName)
+			.toArray(String[]::new);
 	}
 
 	@NotNull
 	@Override
-	public SettingsEditor<? extends RunConfiguration> getConfigurationEditor() {
-		return new ServiceFabricSettingsEditor();
+	public SettingsEditor<ServiceFabricRunConfiguration> getConfigurationEditor() {
+		return new ServiceFabricSettingsEditor(suggestedSfProjFolder, publishProfileOptions);
 	}
 
 	@Override
--- src/dev/pitlor/rider_service_fabric_support/ServiceFabricSettingsEditor.java
+++ src/dev/pitlor/rider_service_fabric_support/ServiceFabricSettingsEditor.java
@@ -2,50 +2,41 @@ package dev.pitlor.rider_service_fabric_support;
 
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.ui.ComboBox;
-import com.intellij.openapi.ui.LabeledComponent;
-import com.intellij.openapi.ui.VerticalFlowLayout;
-import com.intellij.ui.ComboboxWithBrowseButton;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.util.ui.FormBuilder;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
 public class ServiceFabricSettingsEditor extends SettingsEditor<ServiceFabricRunConfiguration> {
-	private JPanel formContainer;
-	private LabeledComponent<ComboboxWithBrowseButton> sfprojFolder;
-	private LabeledComponent<ComboBox<String>> publishProfile;
+	private final TextFieldWithBrowseButton sfprojFolder;
+	private final ComboBox<String> publishProfile;
+
+	public ServiceFabricSettingsEditor(String suggestedSfProjFolder, String[] publishProfileOptions) {
+		sfprojFolder = new TextFieldWithBrowseButton(new JTextField(suggestedSfProjFolder));
+		publishProfile = new ComboBox<>(publishProfileOptions);
+		publishProfile.setMinimumAndPreferredWidth(1000);
+	}
 
 	@Override
 	protected void resetEditorFrom(@NotNull ServiceFabricRunConfiguration runConfiguration) {
-		sfprojFolder.getComponent().getComboBox().setSelectedItem(runConfiguration.sfProjFolder);
-		publishProfile.getComponent().setItem(runConfiguration.publishProfile);
+		sfprojFolder.setText(runConfiguration.sfProjFolder);
+		publishProfile.setItem(runConfiguration.publishProfile);
 	}
 
 	@Override
 	protected void applyEditorTo(@NotNull ServiceFabricRunConfiguration runConfiguration) {
-		runConfiguration.sfProjFolder = (String) sfprojFolder.getComponent().getComboBox().getSelectedItem();
-		runConfiguration.publishProfile = publishProfile.getComponent().getItem();
+		runConfiguration.sfProjFolder = sfprojFolder.getText();
+		runConfiguration.publishProfile = publishProfile.getItem();
 	}
 
 	@NotNull
 	@Override
 	protected JComponent createEditor() {
-		return formContainer;
-	}
-
-	private void createUIComponents() {
-		formContainer = new JPanel();
-		formContainer.setLayout(new VerticalFlowLayout());
-
-		sfprojFolder = new LabeledComponent<>();
-		sfprojFolder.setText("Service Fabric project folder");
-		sfprojFolder.setLabelLocation("West");
-		sfprojFolder.setComponent(new ComboboxWithBrowseButton());
-		formContainer.add(sfprojFolder);
-
-		publishProfile = new LabeledComponent<>();
-		publishProfile.setText("Publish profile");
-		publishProfile.setLabelLocation("West");
-		publishProfile.setComponent(new ComboBox<>());
-		formContainer.add(publishProfile);
+		return new FormBuilder()
+			.setAlignLabelOnRight(false)
+			.addLabeledComponent("Service Fabric project folder", sfprojFolder)
+			.addLabeledComponent("Publish profile", publishProfile)
+			.getPanel();
 	}
 }
