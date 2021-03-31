637f1a7033c3f8b945f192451b9d267d8c5ee372
Jordan Pitlor <jordan.pitlor@avasure.com>
2021-03-31 09:05:02 -0400

    improve form

--- src/dev/pitlor/rider_service_fabric_support/ServiceFabricRunConfiguration.java
+++ src/dev/pitlor/rider_service_fabric_support/ServiceFabricRunConfiguration.java
@@ -7,54 +7,22 @@ import com.intellij.execution.configurations.RunProfileState;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ProjectRootManager;
-import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Arrays;
-import java.util.Optional;
-
 public class ServiceFabricRunConfiguration extends RunConfigurationBase<RunProfileState> {
-	final private String suggestedSfProjFolder;
-	final private String[] publishProfileOptions;
 
 	public String sfProjFolder;
 	public String publishProfile;
 
 	protected ServiceFabricRunConfiguration(Project project, ConfigurationFactory factory, String name) {
 		super(project, factory, name);
-
-		Optional<VirtualFile> maybeSfFolder = Arrays.stream(ProjectRootManager.getInstance(project).getContentRoots())
-			.filter(d -> d.isDirectory() && Arrays.stream(d.getChildren()).anyMatch(f -> f.getName().endsWith(".sfproj")))
-			.findFirst();
-
-		if (maybeSfFolder.isEmpty()) {
-			// There isn't a SF project in the open project
-			suggestedSfProjFolder = "";
-			publishProfileOptions = new String[] {};
-			return;
-		}
-
-		VirtualFile sfFolder = maybeSfFolder.get();
-		suggestedSfProjFolder = sfFolder.getName();
-
-		VirtualFile publishProfiles = sfFolder.findChild("PublishProfiles");
-		if (publishProfiles == null || !publishProfiles.isDirectory()) {
-			// There is a SF project, but it is malformed
-			publishProfileOptions = new String[] {};
-			return;
-		}
-
-		publishProfileOptions = Arrays.stream(publishProfiles.getChildren())
-			.map(VirtualFile::getName)
-			.toArray(String[]::new);
 	}
 
 	@NotNull
 	@Override
 	public SettingsEditor<ServiceFabricRunConfiguration> getConfigurationEditor() {
-		return new ServiceFabricSettingsEditor(suggestedSfProjFolder, publishProfileOptions);
+		return new ServiceFabricSettingsEditor(getProject());
 	}
 
 	@Override
--- src/dev/pitlor/rider_service_fabric_support/ServiceFabricSettingsEditor.java
+++ src/dev/pitlor/rider_service_fabric_support/ServiceFabricSettingsEditor.java
@@ -1,21 +1,72 @@
 package dev.pitlor.rider_service_fabric_support;
 
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.options.SettingsEditor;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.ComboBox;
-import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.TextFieldWithHistory;
+import com.intellij.ui.TextFieldWithHistoryWithBrowseButton;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.FormBuilder;
+import com.intellij.util.ui.SwingHelper;
+import com.intellij.webcore.ui.PathShortener;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.util.Collections;
+import java.util.List;
 
 public class ServiceFabricSettingsEditor extends SettingsEditor<ServiceFabricRunConfiguration> {
-	private final TextFieldWithBrowseButton sfprojFolder;
+	private final TextFieldWithHistoryWithBrowseButton sfprojFolder;
 	private final ComboBox<String> publishProfile;
+	private final JPanel rootPanel;
 
-	public ServiceFabricSettingsEditor(String suggestedSfProjFolder, String[] publishProfileOptions) {
-		sfprojFolder = new TextFieldWithBrowseButton(new JTextField(suggestedSfProjFolder));
-		publishProfile = new ComboBox<>(publishProfileOptions);
-		publishProfile.setMinimumAndPreferredWidth(1000);
+	public ServiceFabricSettingsEditor(Project project) {
+		sfprojFolder = makeSfProjFolderTextField(project);
+		publishProfile = makePublishProfileComboBox();
+		rootPanel = new FormBuilder()
+			.setAlignLabelOnRight(false)
+			.addLabeledComponent("Service Fabric project folder", sfprojFolder)
+			.addLabeledComponent("Publish profile", publishProfile)
+			.getPanel();
+	}
+
+	private TextFieldWithHistoryWithBrowseButton makeSfProjFolderTextField(Project project) {
+		TextFieldWithHistoryWithBrowseButton result = new TextFieldWithHistoryWithBrowseButton();
+		TextFieldWithHistory textFieldWithHistory = result.getChildComponent();
+		List<VirtualFile> sfFolders = SFUtil.getSFFolders(project);
+
+		textFieldWithHistory.setHistorySize(-1);
+		textFieldWithHistory.setMinimumAndPreferredWidth(0);
+		textFieldWithHistory.setHistory(Collections.singletonList(""));
+		PathShortener.enablePathShortening(textFieldWithHistory.getTextEditor(), null);
+		SwingHelper.addHistoryOnExpansion(textFieldWithHistory, () -> {
+			textFieldWithHistory.setHistory(Collections.emptyList());
+			List<String> newFilePaths = ContainerUtil.map(sfFolders, folder -> {
+				String path = FileUtil.toSystemDependentName(folder.getPath());
+				return FileUtil.getLocationRelativeToUserHome(path, false);
+			});
+			Collections.sort(newFilePaths);
+			return newFilePaths;
+		});
+
+		SwingHelper.installFileCompletionAndBrowseDialog(
+			project,
+			sfprojFolder,
+			"Service Fabric Project Folder",
+			FileChooserDescriptorFactory.createSingleFolderDescriptor()
+		);
+
+		return result;
+	}
+
+	private ComboBox<String> makePublishProfileComboBox() {
+		ComboBox<String> result = new ComboBox<>();
+		result.setMinimumAndPreferredWidth(1000);
+
+		return result;
 	}
 
 	@Override
@@ -33,10 +84,6 @@ public class ServiceFabricSettingsEditor extends SettingsEditor<ServiceFabricRun
 	@NotNull
 	@Override
 	protected JComponent createEditor() {
-		return new FormBuilder()
-			.setAlignLabelOnRight(false)
-			.addLabeledComponent("Service Fabric project folder", sfprojFolder)
-			.addLabeledComponent("Publish profile", publishProfile)
-			.getPanel();
+		return rootPanel;
 	}
 }
