be1893c5f31e769bc852a6f46a282d7346d92139
Jordan Pitlor <jordan.pitlor@avasure.com>
2021-04-20 15:06:51 -0400

    add file type

---
+++ src/dev/pitlor/rider_service_fabric_support/file_types/SfFileType.java
@@ -0,0 +1,38 @@
+package dev.pitlor.rider_service_fabric_support.file_types;
+
+import com.intellij.lang.xml.XMLLanguage;
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import dev.pitlor.rider_service_fabric_support.Bundle;
+import icons.PluginIcons;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class SfFileType extends LanguageFileType {
+	protected SfFileType() {
+		super(XMLLanguage.INSTANCE);
+	}
+
+	@NotNull
+	@Override
+	public String getName() {
+		return Bundle.string("file_type.name");
+	}
+
+	@NotNull
+	@Override
+	public String getDescription() {
+		return Bundle.string("file_type.description");
+	}
+
+	@NotNull
+	@Override
+	public String getDefaultExtension() {
+		return Bundle.string("file_type.extension");
+	}
+
+	@Override
+	public Icon getIcon() {
+		return PluginIcons.ServiceFabricLogo;
+	}
+}
